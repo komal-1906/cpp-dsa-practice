efc2a05ec3f0f755ba27b278d8521bd695555a26
komal-1906 <komal1906k@gmail.com>
2026-04-10 18:29:01 +0530

    Patterns added

--- Patterns/Patterns2.cpp
+++ Patterns/Patterns2.cpp
@@ -193,5 +193,119 @@ int main()
 
 
     // Pattern 16
-    
+    // for(int i=0; i<n; i++){
+    //     char ch = 'A'+i;
+    //     for(int j=0; j<=i; j++){
+    //         cout << ch << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    // Pattern 17
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<n-i-1; j++){
+    //         cout << " " << " ";
+    //     }
+
+    //     char ch = 'A';
+    //     int breakpoint = (2*i+1)/2;
+    //     for(int j=1; j<=2*i+1; j++){
+    //         cout << ch << " ";
+    //         if(j<=breakpoint) ch++;
+    //         else ch--;
+    //     }
+
+    //     for(int j=0; j<n-i-1; j++){
+    //         cout << " " << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    // Pattern 18
+    // for(int i=0; i<n; i++){
+    //     for(char ch='E'-i; ch<='E'; ch++){
+    //         cout << ch << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    // Pattern 19
+    // int s=0;
+    // for(int i=0; i<n; i++){
+    //     for(int j=1; j<=n-i; j++){
+    //         cout << "*";       
+    //     }
+    //     for(int j=0; j<s; j++){
+    //         cout << " ";
+    //     }
+    //     for(int j=1; j<=n-i; j++){
+    //         cout << "*";
+    //     }
+    //     s += 2;
+    //     cout << endl;
+    // }
+    // s = 2*n-2;
+    // for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=i; j++){
+    //         cout << "*";       
+    //     }
+    //     for(int j=0; j<s; j++){
+    //         cout << " ";
+    //     }
+    //     for(int j=1; j<=i; j++){
+    //         cout << "*";
+    //     }
+    //     s -= 2;
+    //     cout << endl;
+    // }
+
+
+    // Pattern 20
+    // int spaces = 2*n-2;
+    // for(int i=1; i<=2*n-1; i++){
+    //     int stars = i;
+    //     if(i>n) stars = 2*n-i;
+
+    //     for(int j=1; j<=stars; j++){
+    //         cout << "*";
+    //     }
+    //     for(int j=1; j<=spaces; j++){
+    //         cout << " ";
+    //     }
+    //     for(int j=1; j<=stars; j++){
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    //     if(i<n) spaces -= 2;
+    //     else spaces += 2;
+    // }
+
+
+    // Pattern 21
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<n; j++){
+    //         if(i==0 || j==0 || i==n-1 || j==n-1){
+    //             cout << "*" << " ";
+    //         }
+    //         else cout << " " << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    // Pattern 22
+    // for(int i=0; i<2*n-1; i++){
+    //     for(int j=0; j<2*n-1; j++){
+    //         int top = i;
+    //         int left = j;
+    //         int right =(2*n-2) - j;
+    //         int down =(2*n-2) - i;
+    //         cout << (n-min(min(top,down), min(left, right))) << " ";
+    //     }
+    //     cout << endl;
+    // }
+
 }
