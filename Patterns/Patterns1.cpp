72a158f1220f7137467a11971fada461dcbdc688
komal-1906 <komal1906k@gmail.com>
2026-04-10 18:31:31 +0530

    Debugged patterns

--- Patterns/Patterns1.cpp
+++ Patterns/Patterns1.cpp
@@ -7,250 +7,250 @@ int main()
     cin >> n;
 
     // Pattern 1
-     int i = 1;
-     while (i <= n){
-         int j = 1;
-         while (j<=n){
-             cout << "*";
-             j++;
-         }
-         cout << endl;
-         i++;
-     }
-     return 0;
+    //  int i = 1;
+    //  while (i <= n){
+    //      int j = 1;
+    //      while (j<=n){
+    //          cout << "*";
+    //          j++;
+    //      }
+    //      cout << endl;
+    //      i++;
+    //  }
+    //  return 0;
 
 
     // Pattern 2
-     int i =1;
-     while (i<=n){
-         int j = 1;
-         while (j<=n){
-             cout << i;
-             j++;
-         }
-         cout << endl;
-         i++;
-     }
-     return 0;
+    //  int i =1;
+    //  while (i<=n){
+    //      int j = 1;
+    //      while (j<=n){
+    //          cout << i;
+    //          j++;
+    //      }
+    //      cout << endl;
+    //      i++;
+    //  }
+    //  return 0;
 
 
     // Pattern 3
-     int i =1;
-     while(i<=n){
-         int j = 1;
-         while (j<=n){
-             cout<< j;
-             j++;
-         }
-         cout << endl;
-         i++;
-     }
-     return 0;
+    //  int i =1;
+    //  while(i<=n){
+    //      int j = 1;
+    //      while (j<=n){
+    //          cout<< j;
+    //          j++;
+    //      }
+    //      cout << endl;
+    //      i++;
+    //  }
+    //  return 0;
 
 
     // Pattern 4
-     int i =1;
-     int count = 1;
-     while(i<=n){
-         int j =1;
-         while (j<=n){
-             cout << count << " ";
-             count = count + 1;
-             j++;
-         }
-     cout << endl;
-     i++;
-     }
-     return 0;
+    //  int i =1;
+    //  int count = 1;
+    //  while(i<=n){
+    //      int j =1;
+    //      while (j<=n){
+    //          cout << count << " ";
+    //          count = count + 1;
+    //          j++;
+    //      }
+    //  cout << endl;
+    //  i++;
+    //  }
+    //  return 0;
 
 
     // Pattern 5
-    int i = 1;
-    while (i <= n)
-    {
-        int j = 1;
-        while (j <= i)
-        {
-            cout << " * ";
-            j++;
-        }
-        cout << endl;
-        i++;
-    }
-    return 0;
+    // int i = 1;
+    // while (i <= n)
+    // {
+    //     int j = 1;
+    //     while (j <= i)
+    //     {
+    //         cout << " * ";
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+    // return 0;
 
 
     // Pattern 6
-    int i = 1;
-    while (i<=n){
-        int j = 1;
-        while (j<=i){
-            cout << i << " ";
-            j++;
-        }
-        cout << endl;
-        i++;
-    }
-    return 0;
+    // int i = 1;
+    // while (i<=n){
+    //     int j = 1;
+    //     while (j<=i){
+    //         cout << i << " ";
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+    // return 0;
 
 
     // Pattern 7 
-     int i = 1;
-     int value = i;
-     while (i<=n){
-         int j = 1;
-         while (j<=i){
-             cout << value << " ";
-             value++;
-             j++;
-         }
-         cout << endl;
-         i++;
-     }
-     return 0;
+    //  int i = 1;
+    //  int value = i;
+    //  while (i<=n){
+    //      int j = 1;
+    //      while (j<=i){
+    //          cout << value << " ";
+    //          value++;
+    //          j++;
+    //      }
+    //      cout << endl;
+    //      i++;
+    //  }
+    //  return 0;
 
 
     // Pattern 8
-     int i = 1;
-     while (i<=n){
-         int j = 1;
-         int value = i;
-         while (j<=i){
-             cout << value << " ";
-             value++;
-             j++;
-         }
-         cout << endl;
-         i++;
-     }
-     return 0;
+    //  int i = 1;
+    //  while (i<=n){
+    //      int j = 1;
+    //      int value = i;
+    //      while (j<=i){
+    //          cout << value << " ";
+    //          value++;
+    //          j++;
+    //      }
+    //      cout << endl;
+    //      i++;
+    //  }
+    //  return 0;
 
 
     // Pattern 9
-     int i =1;
-     while (i<=n){
-         int j= i;
-         while (j>=1){
-             cout<< j<< " ";
-             j--;
-         }
-         cout << endl;
-         i++;
-     }
-     return 0;
+    //  int i =1;
+    //  while (i<=n){
+    //      int j= i;
+    //      while (j>=1){
+    //          cout<< j<< " ";
+    //          j--;
+    //      }
+    //      cout << endl;
+    //      i++;
+    //  }
+    //  return 0;
 
 
     // Pattern 10
-     int i = 1;
-     while (i<=n){
-         char j =1;
-         while (j<=n){
-             cout << char('A'+i-1);
-             j++;
-         }
-         cout << endl;
-         i++;
-     }
-     return 0;
+    //  int i = 1;
+    //  while (i<=n){
+    //      char j =1;
+    //      while (j<=n){
+    //          cout << char('A'+i-1);
+    //          j++;
+    //      }
+    //      cout << endl;
+    //      i++;
+    //  }
+    //  return 0;
 
 
     // Pattern 11
-     int i = 1;
-     while (i<=n){
-         int j =1;
-         while (j<=n){
-             cout << char('A'+i+j-2);
-             j++;
-         }
-         cout << endl;
-         i++;
-     }
-     return 0;
+    //  int i = 1;
+    //  while (i<=n){
+    //      int j =1;
+    //      while (j<=n){
+    //          cout << char('A'+i+j-2);
+    //          j++;
+    //      }
+    //      cout << endl;
+    //      i++;
+    //  }
+    //  return 0;
 
 
     // Pattern 12
-        int i = 1;
-        while (i <= n)
-        {
-            int j = 1;
-            char start = 'A' + n - i;
-            while (j <= i)
-            {
-                cout << start;
-                start++;
-                j++;
-            }
-            cout << endl;
-            i++;
-        }
-        return 0;
+    //     int i = 1;
+    //     while (i <= n)
+    //     {
+    //         int j = 1;
+    //         char start = 'A' + n - i;
+    //         while (j <= i)
+    //         {
+    //             cout << start;
+    //             start++;
+    //             j++;
+    //         }
+    //         cout << endl;
+    //         i++;
+    //     }
+    //     return 0;
 
 
     // Pattern 13
-    int i = 1;
-    while (i<=n){
-        int space = n -i;
-        while (space){
-            cout << " ";
-            space--;
-        }
+    // int i = 1;
+    // while (i<=n){
+    //     int space = n -i;
+    //     while (space){
+    //         cout << " ";
+    //         space--;
+    //     }
  
-        int j = 1;
-        while (j<=i){
-            cout << "*";
-            j++;
-        }
-        cout<<endl;
-        i++;
-    }
-    return 0;
+    //     int j = 1;
+    //     while (j<=i){
+    //         cout << "*";
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+    // return 0;
 
 
     // Pattern 14
-    int i =1;
-    while(i<=n){
-        int space = n-i;
-        while(space){
-            cout << " ";
-            space--;
-        }
+    // int i =1;
+    // while(i<=n){
+    //     int space = n-i;
+    //     while(space){
+    //         cout << " ";
+    //         space--;
+    //     }
 
-        int j =1;
-        while (j<=i){
-            cout << j << " ";
-            j++;
-        }
+    //     int j =1;
+    //     while (j<=i){
+    //         cout << j << " ";
+    //         j++;
+    //     }
 
-        int start = i-1;
-        while(start){
-            cout<<start << " ";
-            start--;
-        }
+    //     int start = i-1;
+    //     while(start){
+    //         cout<<start << " ";
+    //         start--;
+    //     }
 
-        cout << endl;
-        i++;
-    }
-    return 0;
+    //     cout << endl;
+    //     i++;
+    // }
+    // return 0;
 
 
     // Pattern 15
-    for(int i=n; i>=1; i--){
+    // for(int i=n; i>=1; i--){
 
-        for(int j=1; j<=i; j++){
-            cout << "* ";
-        }
+    //     for(int j=1; j<=i; j++){
+    //         cout << "* ";
+    //     }
 
-        for(int j=1; j<=2*n-2*i; j++){
-            cout << " ";
-        }
+    //     for(int j=1; j<=2*n-2*i; j++){
+    //         cout << " ";
+    //     }
 
-        for(int j=1; j<=i; j++){
-                cout << "* ";
+    //     for(int j=1; j<=i; j++){
+    //             cout << "* ";
          
-        }
+    //     }
 
-        cout << endl;
-        return 0;
-    }
+    //     cout << endl;
+    //     return 0;
+    // }
 
 }
